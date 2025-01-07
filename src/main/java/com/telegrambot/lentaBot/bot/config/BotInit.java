c69491a4ba666bd7a94f29e5335b0865b3a80dc9
mikl14 <mikl.eli2002@gmail.com>
2025-01-07 20:46:18 +0300

    Система миграции и работа с базой перенесены в другой сервис

--- src/main/java/com/telegrambot/lentaBot/bot/config/BotInit.java
+++ src/main/java/com/telegrambot/lentaBot/bot/config/BotInit.java
@@ -1,24 +1,20 @@
-package com.telegrambot.lentaBot.config;
+package com.telegrambot.lentaBot.bot.config;
 
+
+import com.telegrambot.lentaBot.bot.service.TelegramBot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import com.telegrambot.lentaBot.service.TelegramBot;
 
 @Component
 public class BotInit
 {
-
+    @Autowired
     TelegramBot bot;
-
-    public BotInit(TelegramBot bot)
-    {
-        this.bot = bot;
-    }
-
 
     @EventListener({ContextRefreshedEvent.class})
     public void Init() throws TelegramApiException
